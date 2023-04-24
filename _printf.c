306626b3c27e896e8460ff23567c5e65aaea460a
zagaouch <i.zagaouch@gmail.com>
2023-04-24 12:54:18 -0700

    creat first task

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _printf - function that produces output according to a format.
+ * @format: the format
+ *
+ * Return: the number of characters
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	va_start(args, format);
+	int i;
+	int number;
+	char* str = va_arg(args, char*);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				putchar(va_arg(args, int));
+				number++;
+			}
+			else if (*format == 's')
+			{
+				for (i = 0; str[i] != '\0'; i++)
+				{
+					putchar(str[i]);
+					number++;
+				}
+			}
+			else if (*format == '%')
+			{
+				 putchar('%');
+				 number++;
+			}
+			else
+				return -1;
+		}
+		else
+		{
+			putchar(*format);
+			number++;
+		}
+	}
+	va_end(args);
+	return (number);
+}
